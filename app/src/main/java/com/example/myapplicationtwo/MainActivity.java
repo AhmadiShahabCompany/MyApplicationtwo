cf9c3892f8fb9ffba2ddf8896117c16396a39fe1
mojtaba ahmadi <mojtaba.ahmadi@GHELICHI-LAPTOP.Shahab.com>
2019-08-17 00:28:01 +0330

    for test

--- app/src/main/java/com/example/myapplicationtwo/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtwo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplicationtwo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MYTAG","ONCREATE TAG TEST");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MYTAG","ONSTART TAG TEST");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG","ONRESUME TAG TEST");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MYTAG","ONPAUSE TAG TEST");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MYTAG","ONSTOP TAG TEST");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MYTAG","ONDESTROY TAG TEST");
     }
 }
